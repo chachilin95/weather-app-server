45c5fa50606bbf1dbbff32f5ca5d9e47577a4670
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-10 22:51:30 -0500

    Created handlebar versions of public html pages

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 
 const app = express();
 const publicDir = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDir));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Alejandro Figueroa'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Alejandro Figueroa'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        msg: 'How can I help you?'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
