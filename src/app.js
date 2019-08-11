76cfca1e98a27e38110c5eecba6c5522eb7f8e32
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-10 23:08:41 -0500

    moved hbs files to new templates directory

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDir = path.join(__dirname, '../public');
 
+// define paths for express config
+const publicDir = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// setup static directory to serve
 app.use(express.static(publicDir));
 
 app.get('', (req, res) => {
