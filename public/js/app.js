f66529efa95577886298f202ee074598f5fcabbb
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-11 21:54:53 -0500

    Added html messages and styled app

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,25 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    // reset messages
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
 
     const address = search.value;
     fetch(`http://localhost:42069/weather?address=${address}`).then((res) => {
         res.json().then((data) => {
             if (data.error) {
-                return console.log(data.error);
+                messageOne.textContent = data.error;
+            } else {
+                // set messages
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecastData;
             }
-
-            console.log(data);
         });
     });
 
